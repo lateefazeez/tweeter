a3fb7f1d1e0cf75eadaac6bbe6c606d35a452862
Lateef Azeez <codelate18@gmail.com>
2021-10-05 17:23:52 -0600

    create the tweet layout

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function(e) {
+    const tweetLength = $(this).val().length;
+    const countDown = 140 - tweetLength;
+    let $counter = $(this).parent("form").children(".tweet-control").children(".counter");
+
+    // Make counter turn red color when input length is more than 140
+    if (tweetLength > 140) {
+      $counter.addClass("red");
+    }
+    $counter.val(countDown);
+    
+  });
+});
