7398a55108ed0085a5670d9ad30a7973794682ff
Lateef Azeez <codelate18@gmail.com>
2021-10-06 14:25:45 -0600

    render a single tweet dynamically and display using timeago package

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+$(() => {
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+  
+  const createTweetElement = (tweet) => {
+    const $tweet = $(`<article class="tweet"></article>`);
+    const $header = $(`<header><div><i class="fas fa-user-tie"></i><h4>${tweet.user.name}</h5></div><p>${tweet.user.handle}</p></header>`);
+    const $content = $(`<div><h4>${tweet.content.text}</h4></div>`);
+    const $footer = $(`<footer><h6>${timeago.format(tweet.created_at)}</h6><div><i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div></footer>`);
+    $tweet.append($header, $content, $footer);
+    return $tweet;
+  };
+  
+  const $tweet = createTweetElement(tweetData);
+  console.log($tweet);
+  $('#tweet-container').append($tweet);
+});
