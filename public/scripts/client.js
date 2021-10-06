971260965ead1799e72d3a49aa0bcbbdaf229f99
Lateef Azeez <codelate18@gmail.com>
2021-10-06 14:33:52 -0600

    render multiple tweets from array of tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,16 +6,34 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(() => {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = (tweets) => {
+    return tweets.map(tweet => $("#tweet-container").append(createTweetElement(tweet)));
   };
   
   const createTweetElement = (tweet) => {
@@ -28,7 +46,5 @@ $(() => {
     return $tweet;
   };
   
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('#tweet-container').append($tweet);
+  renderTweets(data);
 });
