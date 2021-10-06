1cde4fa2d79c239d8b5097a3555cb11d7e5def01
Lateef Azeez <codelate18@gmail.com>
2021-10-06 16:22:51 -0600

    fetch data from tweets route and render in ui

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,35 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 $(() => {
   // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+
+  const loadTweets = () => {
+    const url = "http://localhost:8080/tweets";
+
+    //fetch all tweets from the tweets page route
+    $.ajax({
+      url: url,
+      method: "GET"
+    })
+      .then(data => renderTweets(data))
+      .catch(err => console.log(err));
+  };
+  loadTweets();
+
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    const $data = $(this).serialize();
+    // console.log("RAW DATA: ", $(this).serialize());
+    const $inputFiled = $(this).children("#tweet-text");
+    $inputFiled.val("");
+    $inputFiled.focus();
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: $data
+    })
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+  });
 
   const renderTweets = (tweets) => {
     return tweets.map(tweet => $("#tweet-container").append(createTweetElement(tweet)));
@@ -38,7 +43,7 @@ $(() => {
   
   const createTweetElement = (tweet) => {
     const $tweet = $(`<article class="tweet"></article>`);
-    const $header = $(`<header><div><i class="fas fa-user-tie"></i><h4>${tweet.user.name}</h5></div><p>${tweet.user.handle}</p></header>`);
+    const $header = $(`<header><div><img href="${tweet.user.avatars}"><h4>${tweet.user.name}</h5></div><p>${tweet.user.handle}</p></header>`);
     const $content = $(`<div><h4>${tweet.content.text}</h4></div>`);
     const $footer = $(`<footer><h6>${timeago.format(tweet.created_at)}</h6><div><i class="fas fa-flag"></i>
     <i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div></footer>`);
@@ -46,5 +51,5 @@ $(() => {
     return $tweet;
   };
   
-  renderTweets(data);
+  
 });
